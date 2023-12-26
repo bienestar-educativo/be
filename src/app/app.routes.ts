b407feca4ec4b5d1ff965ded97c348655a8b0ccd
jurecode <61298688+jurecode@users.noreply.github.com>
2023-12-26 04:37:07 -0300

    subior

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { IngresaralumnoComponent } from './vistas/ingresaralumno/ingresaralumno.component';
+
+export const routes: Routes = [
+    {path:'', component: IngresaralumnoComponent}
+];
